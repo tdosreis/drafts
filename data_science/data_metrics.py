71690d2464aab0223ae00d2df02a8ea1f8e816a0
Tiago Rosa dos Reis <tiagodosreis@Tiagos-MacBook-Pro.local>
2020-09-12 01:01:01 -0300

    Improves previous metrics code

--- data_science/data_metrics.py
+++ data_science/data_metrics.py
@@ -1,120 +1,105 @@
 import numpy as np
+import itertools
+from scipy import stats
 
-class EvalMetrics():     
-    '''
-    Alternative class for model evaluation: quantifying the quality of predictions
-    
+
+class EvalMetrics():
+    """Alternative class for model performance evaluation. The
+    current implementation allows the computation of the KS test
+    as well as accuracy and confusion matrix computation.
+
     Author: Tiago Rosa dos Reis
-    
+
     Note: code is still under development
-    '''
-    def __init__(self,y_true,y_pred): 
+    """
+
+    def __init__(self, y_true, y_pred):
         self.y_true = np.array(y_true)
         self.y_pred = np.array(y_pred)
-        
-    def ComputeKS(self): 
+
+    def kolmogorov_smirnov(self):
         '''
-        Computes the Kolmogorov-Smirnov test for an ML model
+        Computes the Kolmogorov-Smirnov test between y_true and y_pred
         '''
-        from scipy import stats
-        return stats.ks_2samp(self.y_pred[self.y_true==1.0],self.y_pred[self.y_true==0.0])[0]
-        
-    def ComputeAccuracy(self): 
+        ks_ = stats.ks_2samp(
+                self.y_pred[self.y_true == 1.0],
+                self.y_pred[self.y_true == 0.0]
+                )[0]
+        return ks_
+
+    def accuracy(self):
         '''
-        Alternative approach to compute the accuracy of a model
+        Computes the accuracy between y_true and y_pred
         '''
-        f = lambda x: 1 if x[0] == x[1] else 0
-        x = zip(self.y_true,self.y_pred)
-        trues = list(map(f,x))
+        x = zip(self.y_true, self.y_pred)
+        trues = list(map(
+            lambda x: 1 if x[0] == x[1] else 0, x))
         return sum(trues)/len(trues)
 
-    def ZeroMatrix(self): 
-        '''
-        Computes the skeleton matrix for all lists of combinations
-        '''
-        import numpy as np
-
-        dim = len(np.unique(self.y_true))
-
-        return np.zeros((1,dim,dim))[0] 
-
-    def Combinations(self,n_combinations=2): 
-        '''
-        Computes all combinations in a list of values
-        '''
-        import itertools
-        import numpy as np
-
-        list_of_values = np.unique(self.y_true)
-
-        # identity combinations
-        a = list(map(lambda x: (x,x),list_of_values))
-
-        # ordered combinations
-        b = list(itertools.combinations(list_of_values,n_combinations))
-
-        # reverse order combinations
-        c = list(map(lambda x: tuple(sorted(x,reverse=True)),b)) 
-
-        return sorted(sum([a,b,c],[]))
-
-    def MapIndices(self): 
-        '''
-        Computes the indices of each value in a vector 
-        '''
-        return {j:i for (i,j) in enumerate(np.unique(self.y_true))}
-
-    def GetIndices(self,x):
-        '''
-        Extracts the indices in a list of tuples 
-        '''
-        ix = self.MapIndices()
-        return tuple([ix.get(x[i]) for i in range(len(x))])
-
-    def Transform(self,n_combinations=2):
-        '''
-        Converts the combination of values to a combination of indices
-        '''
-        return {i: self.GetIndices(i) for i in self.Combinations(n_combinations=2)}
-
-    def SumMaps(self): 
-        '''
-        Computes the summation of values for each key in a map
-        '''
-        return {i:sum(list(map(lambda x: x == i, list(zip(self.y_true,self.y_pred))))) for i in self.Combinations()}
-
-    def ConvertMaps(self): 
-        '''
-        Converts a map of tuples into a map of indices
-        '''
-        ix          = self.MapIndices()
-        coordinates = self.Transform()
-        sum_maps    = self.SumMaps()
-        return {coordinates.get(i): sum_maps.get(i) for i in sum_maps}
-
-    def ConfusionMatrix(self): 
+    def confusion_matrix(self):
         '''
         Generalized form of the confusion matrix between y_true and y_pred
         '''
-        map_to_map = self.ConvertMaps()
+        map_to_map = self._convert_maps()
 
-        M = self.ZeroMatrix()
-        
-        for i in map_to_map: 
+        M = self._zero_matrix()
+
+        for i in map_to_map:
             M[i] = map_to_map.get(i)
 
         return M
 
-    def ConvertToBinary(self,thresh=.5): 
-        '''
-        Converts a list of continuous probabilities into discrete (binary) values
-        '''
-        return list(map(lambda x: 1 if x > thresh else 0, self.y_true))
+    def _convert_maps(self):
+        coordinates = self._transform()
+        sum_maps = self._sum_maps()
+        return {coordinates.get(i): sum_maps.get(i) for i in sum_maps}
 
-    def RandomProbs(self,length): 
-        '''
-        Generates a list of random probabilty values
-        '''
-        return [random.random() for i in range(length)]
-    
-    
+    def _zero_matrix(self):
+
+        dim = len(np.unique(self.y_true))
+
+        return np.zeros((1, dim, dim))[0]
+
+    def _combinations(self, n_combinations=2):
+
+        list_of_values = np.unique(self.y_true)
+
+        # identity combinations
+        a = list(map(lambda x: (x, x), list_of_values))
+
+        # ordered combinations
+        b = list(itertools.combinations(list_of_values, n_combinations))
+
+        # reverse order combinations
+        c = list(map(lambda x: tuple(sorted(x, reverse=True)), b))
+
+        return sorted(sum([a, b, c], []))
+
+    def _map_indices(self):
+
+        return {j: i for (i, j) in enumerate(np.unique(self.y_true))}
+
+    def _get_indices(self, x):
+
+        ix = self._map_indices()
+        return tuple([ix.get(x[i]) for i in range(len(x))])
+
+    def _transform(self, n_combinations=2):
+
+        return {i: self._get_indices(i)
+                for i in self._combinations(n_combinations=2)}
+
+    def _sum_maps(self):
+
+        maps = {
+                i: sum(
+                    list(
+                        map(
+                            lambda x: x == i,
+                            list(zip(self.y_true, self.y_pred))
+                            )
+                        )
+                    )
+                for i in self._combinations()}
+
+        return maps
